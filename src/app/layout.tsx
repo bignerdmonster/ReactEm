eb2ccdcb45641d7d68ee06f84d97f73ed150f996
⌬ Felix ⌬ <s216-felix.bryan@k12.hi.us>
2024-09-12 09:00:58 -1000

    shameless plagurism of daisyUI navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,12 +15,29 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
       <body>
+      <div className="navbar bg-base-100">
+  <div className="flex-1">
+    <a className="btn btn-ghost text-xl">ReactEm</a>
+  </div>
+  <div className="flex-none">
+    <ul className="menu menu-horizontal px-1">
+      <li><a>Link</a></li>
+      <li>
+        <details>
+          <summary>Parent</summary>
+          <ul className="bg-base-100 rounded-t-none p-2">
+            <li><a>Link 1</a></li>
+            <li><a>Link 2</a></li>
+          </ul>
+        </details>
+      </li>
+    </ul>
+  </div>
+</div>
         <div className="w-full">
-          <h1 className="mx-auto text-center text-rose-700 text-obese font-bold hover:underline">tailwind css could only be crafted by the devil himself</h1>
+          <h1 className="mx-auto text-center text-rose-700 text-obese font-bold hover:underline">Now THIS is epic!</h1>
         </div>
-        <div className="w-full">
-          <button className="btn mx-auto">Button</button>
-        </div>
+        
         {children}
         <SpeedInsights />
         </body>
