cbf8d7c46018d5f6fae2f03fe0f0fe517831d7ed
⌬ Felix ⌬ <s216-felix.bryan@k12.hi.us>
2024-09-30 09:01:51 -1000

    fooling around

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import { db } from "~/server/db";
-import { renderToString } from 'react-dom/server';
 
 export default async function HomePage() {
   const images = await db.query.images.findMany(); {/*await holds off the async function from killing itself as a way to pass the time it takes for the db to query, 
@@ -10,18 +9,18 @@ export default async function HomePage() {
       <div className="mx-auto w-1/5 py-10 content-center text-center">
         <h1 className="text-2xl text-sky-700 bg-gradient-to-l from-fuchsia-300/85 to-cyan-700/95 underline decoration-double rounded-md hover:blur-lg"> chill w this rn</h1>
       </div>
-      <div className="justify-center flex flex-wrap items-center gap-16">
+      <div className="justify-center flex flex-wrap items-center gap-16 overflow-visible">
         {images.map((image) =>(
-          <div key={image.id} className="group flex flex-col flex-shrink w-48 p-6 bg-gradient-to-r from-cyan-500 to-fuchsia-500 hover:scale-125">
-            <img src={image.url} alt={image.name} title={image.name}/>
-            <div className="text-orange-600 text-lg text-center group-hover:font-black">{image.name}</div>
+          <div key={image.id} className="group flex flex-col flex-shrink w-48 p-6 bg-gradient-to-r from-cyan-500 to-fuchsia-500 hover:scale-150">
+            <img className="group-hover:animate-bounce"src={image.url} alt={image.name} title={image.name}/>
+            <div className="text-orange-600 text-lg text-center group-hover:font-black group-hover:underline group-hover:text-">{image.name}</div>
             <div className= "text-xs text-slate-800 blur-md group-hover:blur-0">Created on {image.createdAt.toString()}</div>
           </div>
         ))}
       </div>
       <br></br>
       <div className="prose">
-        <h1> A <strong></strong>little thing </h1>
+        <h1> A little thing </h1>
         <p> Yuppers, this is cool! </p>
       </div>
     </main>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,21 @@ export default {
       backgroundImage: {
         'him': "url('https://mhs-cte.weebly.com/uploads/1/2/1/9/121936271/published/sta-sta-hanagami-blaise.jpg?1571943510')",
       },
+      animation:{
+        'shook': '1s linear infinite',
+      },
+      keyframes: {
+        'shook' : {
+          '10%, 90%': {
+            transform: 'translate3d(-1px, 0, 0)'},
+          '20%, 80%' : {
+            transform: 'translate3d(2px, 0, 0)'},
+          '30%, 50%, 70%': { 
+            transform: 'translate3d(-4px, 0, 0)'},
+          '40%, 60%': {
+            transform: 'translate3d(4px, 0, 0)'},
+        },
+      },
     },
   },
   plugins: [
