8015c8d548b24826255e6cdb8bd4a0a9f827726c
⌬ Felix ⌬ <s216-felix.bryan@k12.hi.us>
2024-10-21 09:30:56 -1000

    the start of the authentication journey

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -8,6 +8,7 @@ import {
   serial,
   timestamp,
   varchar,
+  json
 } from "drizzle-orm/pg-core";
 
 /**
@@ -33,5 +34,23 @@ export const images = createTable(
   },
   (example) => ({
     nameIndex: index("name_idx").on(example.name),
+  })
+);
+export const squidwarb = createTable(
+  "squidwarb",
+  {
+    id: serial("id").primaryKey(),
+    userName: varchar("userName", { length: 64 }).notNull(),
+    theCreds: varchar("theCreds", { length: 32}).notNull(),
+    theyStuff: json("theyStuff"),
+    createdAt: timestamp("created_at", { withTimezone: true })
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+    updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
+      () => new Date()
+    ),
+  },
+  (example) => ({
+    userNameIndex: index("userName_idx").on(example.userName),
   })
 );
